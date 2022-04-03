269b1a578979f2925742e2d9e8448a4c7b59dce9
Juliana <99342410+Ju1iana@users.noreply.github.com>
2022-04-04 01:12:40 +0300

    equals / hashCode

--- Java_tasks_OOP/src/u/pankratova/employee/Employee.java
+++ Java_tasks_OOP/src/u/pankratova/employee/Employee.java
@@ -57,6 +57,22 @@ public class Employee  {
         this.percent = percent;
     }
 
+    
+     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id && salary == employee.salary && percent == employee.percent
+                && Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, salary, percent);
+    }
+    
+    
     @Override
     public String toString() {
         return "Employee {" +
