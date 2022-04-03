92b9f6a1fb7b14e1495c1e7979ef6dea034ea4b6
Juliana <99342410+Ju1iana@users.noreply.github.com>
2022-04-04 01:23:45 +0300

    equals / hashCode

--- Java_tasks_OOP_2/src/u/pankratova/myPolynomial/MyPolynomial.java
+++ Java_tasks_OOP_2/src/u/pankratova/myPolynomial/MyPolynomial.java
@@ -96,5 +96,19 @@ public class MyPolynomial {
         }
         return new MyPolynomial(leftCoeffs);
     }
+    
+     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyPolynomial that = (MyPolynomial) o;
+        return Arrays.equals(coeffs, that.coeffs);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(coeffs);
+    }
+    
 
 }
